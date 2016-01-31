932210d1cb71eb971b04039090d3a9ff5f045dce
Aaron Larson <acl@Aarons-MacBook-Pro.local>
2016-01-31 12:10:52 -0800

    Fixed the chown segfault with no args

--- chown-src/chown.c
+++ chown-src/chown.c
@@ -12,7 +12,12 @@
 #include <string.h>
 
 int main(int argc, char **argv, char **env) {
-    if (strncmp(argv[1], "-v", 2) == 0) {
+  if (argv[1] == NULL) {
+    fprintf(stderr, "chown: Error 01 encountered. Not enough arguments.\n");
+    return -1;
+
+  }
+  if (strncmp(argv[1], "-v", 2) == 0) {
         printf("Wildebeest™ 'chown' v0.2\n");
         printf("Copyright © 2013, 2016 Aluminium Computing, Inc\n");
         return 0;
@@ -22,10 +27,6 @@ int main(int argc, char **argv, char **env) {
     return -1;
   }
   struct passwd *pwd;
-  if ((pwd = getpwnam(argv[1])) == NULL) { 
-    fprintf(stderr, "chown: Error 02 encountered. No such user as %s.\n", argv[1]);
-    return -1; 
-  }
-  if (chown(argv[2], pwd->pw_uid, pwd->pw_gid) == -1) 
+    if (chown(argv[2], pwd->pw_uid, pwd->pw_gid) == -1)
     fprintf(stderr, "chown: Error 03 encountered. Insufficient permissions.\n");
 }
